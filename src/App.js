f92f3f0e2a042920c36307784af95bba24094a56
Daniel Poliak <daniel.poliak@erstegroup.com>
2020-05-07 10:32:23 +0200

    some styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todos, setTodos] = useState([])
+
+    const toggleTodo = (index) => {
+      const newTodos = [...todos]
+
+          newTodos[index] = {
+          ...todos[index],
+          complete: !todos[index].complete
+      }
+        setTodos(newTodos)
+    }
+
+
+    return (
+    <div className="container-fluid mt-3">
+      <Form
+          onSubmit={text => setTodos([{text, complete:false}, ...todos])}
+      />
+      <ul className="list-group mt-3 mb-3">
+      {
+              todos.map(({text, complete}, index) => (
+                      <li
+                          key={`${text}-${index}`}
+                          onClick={e=> toggleTodo(index)}
+                          class={`list-group-item ${complete ? 'bg-success text-white' : ''}`}
+                          style={{
+                              textDecoration: complete ? 'line-through' : '',
+                            cursor: "pointer"
+                          }}
+                      >
+                          {text}
+                      </li>
+                  )
+              )
+          }
+      </ul>
+      <button class="btn btn-primary" onClick={e => setTodos([])}>RESET </button>
     </div>
   );
 }
